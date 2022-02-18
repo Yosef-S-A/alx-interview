e8ac4ab73f297b84395594d9d9880e8d4ee841de
Yosef-S-A <yosefsamuel22@gmail.com>
2022-02-18 02:37:14 +0000

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const requestAsync = (url) => {
-  return new Promise((resolve, reject) => {
-    request(url, (err, resp, body) => {
-      (err) ? reject(err) : resolve([resp, body]);
-    });
-  });
-};
-
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  async (err, resp, body) => {
-    if (err) throw err; // TODO don't throw in async?
-    if (resp && resp.statusCode === 200) {
-      for (const url of JSON.parse(body).characters) {
-        await requestAsync(url)
-          .then(([_resp, body]) => console.log(JSON.parse(body).name))
-          .catch((err) => console.log(`Oops! : ${err}`));
-      }
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
     }
-  });
+  }
+});
